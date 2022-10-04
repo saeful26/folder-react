2664d67c051c34178587f823ead48dae91c74446
saeful26 <ipulskinhead@gmail.com>
2022-10-04 22:26:04 +0700

    merubah props functional component

--- src/materi/Eduwork/pembahasan/FunctionalComponent.jsx
+++ src/materi/Eduwork/pembahasan/FunctionalComponent.jsx
@@ -1,9 +1,9 @@
-const FunctionalComponent = ({nama}) => {
+const FunctionalComponent = (props) => {
     
     return (
       <div>
         <h1>Komponen ini menggunakan Functionalcomponent</h1>
-        <h2>Hello {nama},selamat belajar</h2>
+        <h2>Hello {props.nama},selamat belajar wkwkw</h2>
       </div>
 
     )
